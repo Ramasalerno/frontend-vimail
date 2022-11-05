a19bf5ed68b1d6f4ed154b5980304ac0a793ac5c
Rama <salernoramiro94@gmail.com>
2022-11-05 11:53:18 -0300

    Agregados banner body, modificado orden home page, estilos

--- src/components/BannerBody/BannerBody.js
+++ src/components/BannerBody/BannerBody.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import "./BannerBodySoporteStyle.css"
+import "./BannerBodyStyle.css"
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
-
-export const BannerBodySoporte = () => {
+export const BannerBody = () => {
   return (
     <AnimationOnScroll animateIn="animate__fadeIn">
     <div>
       <div className='mb-5 mt-3 contenedorBanner'>
-        <img src={"/imagenes/banner/bannerBody2.jpg"} alt="Imagen banner"></img>
+        <img src={"/imagenes/banner/bannerBody1.jpg"} alt="Imagen banner"></img>
       </div>
     </div>
     </AnimationOnScroll>
+    
   )
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { FlexMarcas } from '../FlexMarcas/FlexMarcas';
 import { Carrousel } from '../Carrousel/Carrousel';
-import { ServiciosCard } from '../ServiciosCard/ServiciosCard';
+
 import { BannerBodySoporte } from '../BannerBodySoporte/BannerBodySoporte';
 import FlexFeatures from '../FlexFeatures/FlexFeatures';
 import { InfoHome } from '../InfoHome/InfoHome';
+import { BannerBody } from '../BannerBody/BannerBody';
+import { ServiciosCard } from '../ServiciosCard/ServiciosCard';
 
 export const HomePage = () => {
   return (
     <>
         <Carrousel/>
         <FlexFeatures/>
-        <ServiciosCard/>
         <InfoHome/>
+        <BannerBody/>
+        <ServiciosCard/>
+
         <BannerBodySoporte/>
         <FlexMarcas/>
     </>
